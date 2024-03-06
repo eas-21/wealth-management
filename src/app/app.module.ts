8f2413d11c5aaa09b584b230262ccc5860232a82
eas-21 <easwariswathi@gmail.com>
2024-03-06 12:24:14 +0530

    scroll top component and service alignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { GalleryComponent } from './core/gallery/gallery.component';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
-
+import { ScrollToTopComponent } from './shared/scroll-to-top/scroll-to-top.component';
+import { BlogComponent } from './core/blog/blog.component';
+import { FueloilComponent } from './core/sbi-services/fueloil/fueloil.component';
+import { VermicompostComponent } from './core/sbi-services/vermicompost/vermicompost.component';
+import { TilesComponent } from './core/sbi-services/tiles/tiles.component';
+import { SbiServicesComponent } from './core/sbi-services/sbi-services.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AdminLayoutComponent,
-  
+    BlogComponent,
     ContactComponent,
     GalleryComponent,
-   
+    ScrollToTopComponent,    
+   FueloilComponent,
+   VermicompostComponent,
+   TilesComponent,
+   SbiServicesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/sbi-services/sbi-services.module.ts
+++ src/app/core/sbi-services/sbi-services.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { SbiServicesComponent } from './sbi-services.component';
-import { FueloilComponent } from './fueloil/fueloil.component';
-import { VermicompostComponent } from './vermicompost/vermicompost.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { TilesComponent } from './tiles/tiles.component';
-
 
 @NgModule({
-  declarations: [SbiServicesComponent, FueloilComponent, VermicompostComponent, TilesComponent],
+  declarations: [],
   imports: [
     CommonModule,
     CarouselModule,
